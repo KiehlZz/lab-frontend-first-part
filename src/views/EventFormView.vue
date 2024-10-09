211270534b50421f4c30fe72d62851d55e2d77ab
Sorod Yawa <sorod_yawa@cmu.ac.th>
2024-10-09 18:59:55 +0700

    .

--- src/views/EventFormView.vue
+++ src/views/EventFormView.vue
@@ -15,30 +15,32 @@ const event = ref<Event>({
 </script>
 
 <template>
-    <div class="form-container">
+    <div>
+    <!-- <div class="form-container"> -->
         <h1>Create an Event</h1>
-        <form class="event-form">
-            <div class="form-group">
+        <form>
+        <!-- <form class="event-form"> -->
+            <!-- <div class="form-group"> -->
                 <label>Category</label>
                 <input v-model="event.category" type="text" placeholder="Category" class="field" />
-            </div>
+            <!-- </div> -->
 
             <h3>Name & Describe Your Event</h3>
-            <div class="form-group">
+            <!-- <div class="form-group"> -->
                 <label>Title</label>
                 <input v-model="event.title" type="text" placeholder="Title" class="field" />
-            </div>
+            <!-- </div> -->
 
-            <div class="form-group">
+            <!-- <div class="form-group"> -->
                 <label>Description</label>
                 <textarea v-model="event.description" placeholder="Description" class="field"></textarea>
-            </div>
+            <!-- </div> -->
 
             <h3>Where is Your Event?</h3>
-            <div class="form-group">
+            <!-- <div class="form-group"> -->
                 <label>Location</label>
                 <input v-model="event.location" type="text" placeholder="Location" class="field" />
-            </div>
+            <!-- </div> -->
 
             <button class="button" type="submit">Submit</button>
         </form>
